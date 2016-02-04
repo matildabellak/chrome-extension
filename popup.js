7bc90d5197953c2d3703a10b847e7f0359ef0f15
Matilda Bellak <matilda.bellak@akqa.com>
2016-02-04 17:28:03 +0100

    Almost working

--- popup.js
+++ popup.js
@@ -2,6 +2,6 @@ function clickHandler() {
 	window.webkitNotifications.createNotification("icon19.png", "Clicked", "It was clicked").show();
 }
 
-function loadHandler() {
-	window.webkitNotifications.createNotification("icon19.png", "Plugin Loaded", "It was loaded").show();
-}
+// function loadHandler() {
+// 	window.webkitNotifications.createNotification("icon19.png", "Plugin Loaded", "It was loaded").show();
+// }
