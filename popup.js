9bd94e98c08a0b7d21be5e4219d11a64f68b9ee6
Matilda Bellak <matilda.bellak@akqa.com>
2016-02-04 18:08:35 +0100

    Changing color

--- popup.js
+++ popup.js
@@ -2,6 +2,16 @@ function clickHandler() {
 	window.webkitNotifications.createNotification("icon19.png", "Clicked", "It was clicked").show();
 }
 
+
+
+
+window.onload = function(){ 
+    document.getElementById('btn_div').onclick = function () {
+    document.getElementById('changed_div').style.backgroundColor = '#242424';
+}
+};
+
+
 // function loadHandler() {
 // 	window.webkitNotifications.createNotification("icon19.png", "Plugin Loaded", "It was loaded").show();
 // }
